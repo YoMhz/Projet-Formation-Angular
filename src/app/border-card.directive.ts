74f9078321115acba8bd73ab08de06eee63d923b
Yohann Mahrez <yohann.mahrez@gmail.com>
2024-03-15 21:10:01 +0100

    feat:
    Entrainement pour modifier des cartes backgroundColor et border radius via la directive

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -6,18 +6,29 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 })
 export class BorderCardDirective {
 
+  private initialColor: string = '#f5f5f5';
+  private defaultColor: string = '#009688';
+  private defaultHeight: number = 180;
+  private initialBackgrounColor: string = '#ffffff'
+  private defaultBackground: string = '#42B2A6'
+
   constructor(private el: ElementRef) {
-    this.setHeight(180);
-    this.setBorder('#f5f5f5')
+    this.setHeight(this.defaultHeight);
+    this.setBorder('#f5f5f5');
+    this.setBorderRadius(15);
+    this.setBackgroundColorCard(this.initialBackgrounColor);
   }
   @Input('pokemonBorderCard') borderColor: string;
 
   @HostListener('mouseenter') onMouseEnter() {
-    this.setBorder(this.borderColor || '#009688');
+    this.setBorder(this.borderColor || this.defaultColor);
+    this.setBackgroundColorCard(this.defaultBackground)
   }
 
   @HostListener('mouseleave') onMouseLeave() {
-    this.setBorder('#f5f5f5');
+    this.setBorder(this.initialColor);
+    this.setBackgroundColorCard(this.initialBackgrounColor)
+
   }
 
   setHeight(height: number) {
@@ -26,5 +37,13 @@ export class BorderCardDirective {
 
   setBorder(color: string) {
     this.el.nativeElement.style.border = `3px solid ${color}`;
+  }
+
+  setBorderRadius(pixel: number) {
+    this.el.nativeElement.style.borderRadius = `${pixel}px`
+  }
+
+  setBackgroundColorCard(color: string) {
+    this.el.nativeElement.style.backgroundColor = `${color}`
   }
 }
