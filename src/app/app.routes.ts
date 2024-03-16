a289e68e7b837cb3c5b4108b9f3e1ec38186a125
Yohann Mahrez <yohann.mahrez@gmail.com>
2024-03-16 18:35:04 +0100

    Mise en place gestion erreur 404 si l'utilisateur se diriger vers une page qui n'existe pas !

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
 import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
+import { PageNotFoundComponent } from './page-note-found/page-note-found.component';
+
+// Angular lit les route du haut vers le bas il y à un sens !!
 
 export const routes: Routes = [
     {path: 'pokemons', component: ListPokemonComponent},
     {path: 'pokemon/:id', component: DetailPokemonComponent},
     {path: '', redirectTo: 'pokemons', pathMatch: "full"},
+    {path: '**', component: PageNotFoundComponent},
 ];
