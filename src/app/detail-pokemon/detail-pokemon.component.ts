4fef41ee520b33cc9d4d7d71e68c5af349f3823e
Yohann Mahrez <yohann.mahrez@gmail.com>
2024-03-16 18:00:25 +0100

    Création du composant et du template pour l'affichage du pokémon avec l'id 3 ainsi que que la récupération de l'id via l'url directement.

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { POKEMONS } from '../mock-pokemon-list';
+import { Pokemon } from '../pokemon';
+import { PokemonTypeColorPipe } from '../pokemon-type-color.pipe';
 
 @Component({
   selector: 'app-detail-pokemon',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, PokemonTypeColorPipe],
   templateUrl: './detail-pokemon.component.html',
   styles: ``
 })
 export class DetailPokemonComponent {
+  pokemonList: Pokemon[];
+  pokemon: Pokemon|undefined;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.pokemonList = POKEMONS;
+    const pokemonId: string|null = this.route.snapshot.paramMap.get('id');
+    if(pokemonId){
+      this.pokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
+    }
+  }
 }
